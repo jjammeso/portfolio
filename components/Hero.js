0e5808fd063413d414a72695653417af05033710
Sonam Jamtsho <sjamtsho@myseneca.ca>
2023-10-08 18:36:26 -0400

    replaced apostrophe

--- components/Hero.js
+++ components/Hero.js
@@ -5,9 +5,9 @@ const Hero = () => {
     <div className="hero-container">
       <Image src='/IMG_6137.JPG' className="profile-img" width={300} height={300} alt="Profile pic" />
       <div className="hero-text">
-        <h1>Hey, I'm Sonam Jamtsho 👋</h1>
+        <h1>Hey, I&aposm Sonam Jamtsho 👋</h1>
         <p>
-          I'm a Fullstack web developer.
+          I&aposm a Fullstack web developer.
         </p>
       </div>
     </div>
