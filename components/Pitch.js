62f8a8019a956fb392ec2c305db2a6d18b47fac2
Sonam Jamtsho <sjamtsho@myseneca.ca>
2023-10-08 19:30:40 -0400

    added video3

--- components/Pitch.js
+++ components/Pitch.js
@@ -1,7 +1,7 @@
 export default function PitchVideo(){
     return (
         <div className="pitch">
-        <iframe width="173" height="97" src="https://www.youtube.com/embed/0fYi8SGA20k" title="Build and Deploy an Amazing 3D Web Developer Portfolio in React JS | Beginner Three.js Tutorial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="693" height="388" src="https://www.youtube.com/embed/0fYi8SGA20k" title="Build and Deploy an Amazing 3D Web Developer Portfolio in React JS | Beginner Three.js Tutorial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       );
 }
