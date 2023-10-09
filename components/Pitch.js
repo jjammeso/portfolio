4d267417cd6b3068c9c2e80a8e76980ce57d79aa
Sonam Jamtsho <sjamtsho@myseneca.ca>
2023-10-09 00:10:11 -0400

    added video update1

--- components/Pitch.js
+++ components/Pitch.js
@@ -1,7 +1,7 @@
 export default function PitchVideo(){
     return (
         <div className="pitch">
-        <iframe width="693" height="390" src="https://www.youtube.com/embed/XzeIb1P5icA" title="pitch video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="866" height="487" src="https://www.youtube.com/embed/XzeIb1P5icA" title="pitch video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       );
 }
