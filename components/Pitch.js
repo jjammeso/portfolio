09d743538e41233f9deb114401c484b08aba0c94
Sonam Jamtsho <sjamtsho@myseneca.ca>
2023-10-09 01:18:00 -0400

    changed the video2djklkjsdfsdf

--- components/Pitch.js
+++ components/Pitch.js
@@ -1,7 +1,9 @@
 export default function PitchVideo(){
     return (
         <div className="pitch">  
-        <iframe width="866" height="487" src="https://www.youtube.com/embed/GUn-VR7hC0c" title="pitch video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <video controls width="640" height="360">
+        <source src="/pitch video.mp4" type="video/mp4" />
+      </video>
         </div>
       );
 }
