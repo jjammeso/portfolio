e373caa07ce2155675ba5de2afd13cd987079a85
Sonam Jamtsho <sjamtsho@myseneca.ca>
2023-10-09 01:04:59 -0400

    changed the video2djklkj

--- components/Pitch.js
+++ components/Pitch.js
@@ -1,7 +1,7 @@
 export default function PitchVideo(){
     return (
         <div className="pitch">  
-        <iframe src="https://seneca-my.sharepoint.com/personal/sjamtsho_myseneca_ca/_layouts/15/embed.aspx?UniqueId=1c435486-c3df-40d5-9fcb-e0f1154f9310&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="pitch video.mp4"></iframe>
+        <iframe width="866" height="487" src="https://www.youtube.com/embed/GUn-VR7hC0c" title="pitch video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       );
 }
