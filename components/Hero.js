4a78ee6b8dbcc05d55f3a561401b3a06861a87ae
Sonam Jamtsho <sjamtsho@myseneca.ca>
2023-10-10 00:57:40 -0400

    changed the image layout

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="hero-container">
       <Image src='/profilepic.jpeg' className="profile-img" width={300} height={300} alt="Profile pic" objectFit="contain"/>
       <div className="hero-text">
-        <h1><span className="span">Hey, I&apos;m Sonam Jamtsho 👋</span></h1>
+        <div><h1><span className="span">Hey, I&apos;m Sonam Jamtsho 👋</span></h1></div>
         <p>
           <span className="span">I&apos;m a Fullstack web developer.</span>
         </p><br/><br/><br/>
